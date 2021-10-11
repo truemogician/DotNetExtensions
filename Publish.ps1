b6a5a618cb06129dbaf53dd73ecc29bbfc9bb082
true_mogician <925269713@qq.com>
2021-10-11 17:56:17 +0800

    feat: latest version

--- Publish.ps1
+++ Publish.ps1
@@ -2,24 +2,50 @@ function Publish-Package([string] $project, [string] $version) {
 	if (!(Test-Path "$project/*.csproj")) {
 		return "Project $project not found";
 	}
+	$csprojPath = Get-ChildItem "$project/*.csproj" -File -Name;
+	$structure = [xml](Get-Content "$project/$csprojPath");
+	$packageId = $structure.Project.PropertyGroup.PackageId;
+	Set-Location "$project/bin/Release";
 	if (!$version) {
-		Set-Location "$project/bin/Release";
-		Invoke-Expression "dotnet nuget push `"*.nupkg`" --source github-personal --skip-duplicate";
-		Set-Location "../../../";
+		$pkgs = [string[]](Get-ChildItem "$packageId.*.nupkg" -File -Name);
+		foreach ($pkg in $pkgs) {
+			Invoke-Expression "dotnet nuget push `"$pkg`" --source github-personal"
+		}
 	}
 	else {
-		if (!($version -match '\d+\.\d+\.\d+')) {
-			return "Wrong version syntax";
+		if ($version -eq 'latest') {
+			$pkgs = [string[]](Get-ChildItem "$packageId.*.nupkg" -File -Name);
+			$major = 0;
+			$minor = 0;
+			$patch = 0;
+			foreach ($pkg in $pkgs) {
+				$match = [Regex]::Match($pkg, "$packageId\.(?<major>\d+)\.(?<minor>\d+)\.(?<fix>\d+)\.nupkg");
+				if ($match.Success -eq $false) {
+					continue;
+				}
+				$curMajor = [int]($match.Groups['major'].Value);
+				$curMinor = [int]($match.Groups['minor'].Value);
+				$curPatch = [int]($match.Groups['fix'].Value);
+				if ($curMajor -gt $major -or ($curMajor -eq $major -and ($curMinor -gt $minor -or ($curMinor -eq $minor -and $curPatch -gt $patch)))) {
+					$major = $curMajor;
+					$minor = $curMinor;
+					$patch = $curPatch;
+				}
+			}
+			Invoke-Expression "dotnet nuget push `"$packageId.$major.$minor.$patch.nupkg`" --source github-personal";
 		}
-		$csprojPath = Get-ChildItem "$project/*.csproj" -File -Name;
-		$structure = [xml](Get-Content "$project/$csprojPath");
-		$packageId = $structure.Project.PropertyGroup.PackageId;
-		$packagePath = "$project/bin/Release/$packageId.$version.nupkg";
-		if (!(Test-Path $packagePath)) {
-			return "$packagePath not found";
+		else {
+			if (!($version -match '\d+\.\d+\.\d+')) {
+				return "Wrong version syntax";
+			}
+			$package = "$packageId.$version.nupkg";
+			if (!(Test-Path $package)) {
+				return "$package not found";
+			}
+			Invoke-Expression "dotnet nuget push `"$package`" --source github-personal";
 		}
-		Invoke-Expression "dotnet nuget push `"$packagePath`" --source github-personal";
 	}
+	Set-Location "../../../";
 	return $true;
 }
 $location = Get-Location;
