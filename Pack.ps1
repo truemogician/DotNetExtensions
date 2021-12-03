7f74b6fa53a3cd332abf0045959e77ea3d470e7b
true_mogician <925269713@qq.com>
2021-12-04 01:49:27 +0800

    feat: overwrite

--- Pack.ps1
+++ Pack.ps1
@@ -13,31 +13,33 @@ else {
 	if (!$projects.Contains($project)) {
 		Write-Output "$project is not a valid project name";
 	}
-	elseif (!('major', 'minor', 'patch').Contains($args[1])) {
+	elseif (!('major', 'minor', 'patch', 'overwrite').Contains($args[1])) {
 		Write-Output "${args[1]} is not a valid version component";
 	}
 	else {
-		$projectFile = [string]((Get-ChildItem "*/$project")[0]);
-		$project = New-Object xml;
-		$project.PreserveWhitespace = $true;
-		$project.Load($projectFile);
-		$curVersion = $project.Project.PropertyGroup.Version;
-		$components = [int[]]($curVersion.Split('.'));
-		switch ($args[1]) {
-			'major' {
-				++$components[0];
-				$components[1] = $components[2] = 0;
+		if ($args[1] -ne 'overwrite') {
+			$projectFile = [string]((Get-ChildItem "*/$project")[0]);
+			$project = New-Object xml;
+			$project.PreserveWhitespace = $true;
+			$project.Load($projectFile);
+			$curVersion = $project.Project.PropertyGroup.Version;
+			$components = [int[]]($curVersion.Split('.'));
+			switch ($args[1]) {
+				'major' {
+					++$components[0];
+					$components[1] = $components[2] = 0;
+				}
+				'minor' {
+					++$components[1];
+					$components[2] = 0;
+				}
+				'patch' {
+					++$components[2];
+				}
 			}
-			'minor' {
-				++$components[1];
-				$components[2] = 0;
-			}
-			'patch' {
-				++$components[2];
-			}
+			$project.Project.PropertyGroup.Version = $components -join '.';
+			$project.Save($projectFile);
 		}
-		$project.Project.PropertyGroup.Version = $components -join '.';
-		$project.Save($projectFile);
 		Invoke-Expression "dotnet pack $($args[0]) -c Release";
 	}
 }
