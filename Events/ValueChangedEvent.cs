8cbe3962a9d3de02a9d25891b908bfb1c72f0e0f
true_mogician <925269713@qq.com>
2021-12-04 16:44:55 +0800

    feat: ValueChangedEvent

---
+++ Events/ValueChangedEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TrueMogician.Extensions.Events {
+	/// <summary>
+	///     Argument of <see cref="ValueChangedEventHandler{T}" />, holding old and new values when some value has already
+	///     changed.
+	/// </summary>
+	/// <typeparam name="T">Type of the changed value</typeparam>
+	public class ValueChangedEventArgs<T> : EventArgs {
+		public ValueChangedEventArgs() { }
+
+		public ValueChangedEventArgs(T? oldValue, T? newValue) {
+			OldValue = oldValue;
+			NewValue = newValue;
+		}
+
+		public new static ValueChangedEventArgs<T> Empty { get; } = new();
+
+		public T? OldValue { get; }
+
+		public T? NewValue { get; }
+	}
+
+	public delegate void ValueChangedEventHandler<T>(object? sender, ValueChangedEventArgs<T> args);
+}
