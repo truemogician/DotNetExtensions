c8825abc791a38eae1c22d3a5e4b36f29c466183
true_mogician <925269713@qq.com>
2021-12-06 19:21:54 +0800

    feat: extract SimpleTreeNode
    chore: rename

---
+++ Collections/Tree/SimpleTreeNode.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TrueMogician.Extensions.Events;
+
+namespace TrueMogician.Extensions.Collections.Tree {
+	/// <summary>
+	///     Base class of basic tree node that provides only the basic structure and functionality of a tree.<br />
+	///     Note that the synchronization between <c>Parent</c> and <c>Children</c> needs to be handled manually.
+	///     For automatic synchronization, refer to <see cref="TreeNode{T}" />
+	/// </summary>
+	/// <typeparam name="T">Type of the derived class</typeparam>
+	public abstract class SimpleTreeNode<T> : IEnumerable<T> where T : SimpleTreeNode<T> {
+		protected internal readonly IList<T> _children;
+
+		private T? _parent;
+
+		protected SimpleTreeNode() : this(null) => _children = new List<T>();
+
+		protected SimpleTreeNode(T? parent) {
+			_children = new List<T>();
+			_parent = parent;
+		}
+
+		protected internal SimpleTreeNode(IList<T> children, T? parent) : this(parent) => _children = children;
+
+		public event ValueChangedEventHandler<T?> ParentChanged = delegate { };
+
+		/// <summary>
+		///     Default enumerator. <see cref="TraversalOrder.PreOrder" /> will be used.
+		/// </summary>
+		public IEnumerator<T> GetEnumerator() => GetEnumerator(TraversalOrder.PreOrder);
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		/// <summary>
+		///     Parent node
+		/// </summary>
+		public virtual T? Parent {
+			get => _parent;
+			set => SetParent(value);
+		}
+
+		/// <summary>
+		///     Child nodes collection
+		/// </summary>
+		public virtual IList<T> Children => _children;
+
+		public bool IsLeaf => _children.Count == 0;
+
+		public bool IsRoot => Parent is null;
+
+		public T Root {
+			get {
+				var root = This;
+				while (!root.IsRoot)
+					root = root.Parent!;
+				return root;
+			}
+		}
+
+		public IEnumerable<T> Ancestors {
+			get {
+				for (var n = Parent; n is not null; n = n.Parent)
+					yield return n;
+			}
+		}
+
+		public IEnumerable<T> Leaves => this.Where(n => n.IsLeaf);
+
+		protected T This => (T)this;
+
+		/// <param name="order">
+		///     Traversal order, possible values are <see cref="TraversalOrder.PreOrder" />,
+		///     <see cref="TraversalOrder.PostOrder" /> and <see cref="TraversalOrder.BreadthFirst" />.
+		///     <see cref="TraversalOrder.InOrder" /> is not supported because this tree
+		///     node isn't binary-guaranteed.
+		/// </param>
+		public IEnumerator<T> GetEnumerator(TraversalOrder order) {
+			switch (order) {
+				case TraversalOrder.InOrder: throw new ArgumentOutOfRangeException(nameof(order), "Non-binary tree cannot be traversed in InOrder");
+				case TraversalOrder.BreadthFirst: {
+					var queue = new Queue<T>();
+					queue.Enqueue(This);
+					while (queue.Count > 0) {
+						var cur = queue.Dequeue();
+						yield return cur;
+						foreach (var child in _children)
+							queue.Enqueue(child);
+					}
+					break;
+				}
+				default: {
+					if (order == TraversalOrder.PreOrder)
+						yield return This;
+					foreach (var child in _children)
+						foreach (var node in child)
+							yield return node;
+					if (order == TraversalOrder.PostOrder)
+						yield return This;
+					break;
+				}
+			}
+		}
+
+		/// <summary>
+		///     Check whether this node is the child of <paramref name="node" />
+		/// </summary>
+		public bool IsChildOf(T node) {
+			var cur = Parent;
+			while (cur is not null && !cur.Equals(node) && !cur.Equals(this))
+				cur = cur.Parent;
+			return node.Equals(cur);
+		}
+
+		/// <summary>
+		///     Check whether this node is the ancestor of <paramref name="node" />
+		/// </summary>
+		public bool IsAncestorOf(T node) => node.IsChildOf(This);
+
+		protected internal void SetParent(T? value) {
+			var old = _parent;
+			_parent = value;
+			ParentChanged(this, new ValueChangedEventArgs<T?>(old, value));
+		}
+	}
+
+	/// <summary>
+	///     Basic tree node. If inheritance required, use <see cref="SimpleTreeNode{T}" /> instead.
+	/// </summary>
+	public sealed class SimpleTreeNode : SimpleTreeNode<SimpleTreeNode> {
+		public SimpleTreeNode() { }
+
+		public SimpleTreeNode(SimpleTreeNode? parent) : base(parent) { }
+	}
+
+	/// <summary>
+	///     Basic tree node with node value. If inheritance required, use <see cref="SimpleTreeNode{T}" /> instead.
+	/// </summary>
+	public sealed class ValuedSimpleTreeNode<T> : SimpleTreeNode<ValuedSimpleTreeNode<T>> {
+		public ValuedSimpleTreeNode(T value) : this(value, null) { }
+
+		public ValuedSimpleTreeNode(T value, ValuedSimpleTreeNode<T>? parent) : base(parent) => Value = value;
+
+		public T Value { get; set; }
+	}
+
+	public enum TraversalOrder : byte {
+		PreOrder,
+
+		InOrder,
+
+		PostOrder,
+
+		BreadthFirst
+	}
+}
---
+++ Collections/Tree/TreeNode.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using TrueMogician.Extensions.Enumerable;
+
+namespace TrueMogician.Extensions.Collections.Tree {
+	/// <summary>
+	///     Common tree node base class that ensures the synchronization between <c>Children</c> and <c>Parent</c>
+	/// </summary>
+	/// <typeparam name="T">
+	///     <inheritdoc />
+	/// </typeparam>
+	public abstract class TreeNode<T> : SimpleTreeNode<T> where T : TreeNode<T> {
+		private bool _updateParentOnChildrenChange = true;
+
+		protected TreeNode() : this(null) { }
+
+		protected TreeNode(T? parent) : base(new ControllableList<T>(), parent) {
+			PrivateChildren.ListChanging += (_, baseArgs) => {
+				switch (baseArgs) {
+					case ControllableListAddingEventArgs<T> addingArgs when ReferenceEquals(addingArgs.Value.Parent, this):
+						baseArgs.Cancel = true;
+						break;
+					case ControllableListAddingRangeEventArgs<T> args:
+						var list = args.Values.Where(node => !ReferenceEquals(node.Parent, this)).ToList();
+						if (list.Count != args.Count) {
+							args.Cancel = true;
+							PrivateChildren.InsertRange(args.Index, list);
+						}
+						break;
+					case ControllableListReplacingEventArgs<T> replacingArgs when ReferenceEquals(replacingArgs.NewValue.Parent, this):                      throw new InvalidOperationException($"The replacing node {replacingArgs.NewValue} already presents in Children");
+					case ControllableListReplacingRangeEventArgs<T> replacingRangeArgs when replacingRangeArgs.NewValues.Any(n => ReferenceEquals(n, this)): throw new InvalidOperationException("Some nodes in the replacing range already present in Children");
+				}
+			};
+			PrivateChildren.ListChanged += (_, baseArgs) => {
+				if (!_updateParentOnChildrenChange)
+					return;
+				switch (baseArgs) {
+					case ControllableListAddedEventArgs<T> args:
+						args.Value.SetParent(This);
+						break;
+					case ControllableListRemovedEventArgs<T> args:
+						args.Value.SetParent(null);
+						break;
+					case ControllableListReplacedEventArgs<T> args:
+						if (!ReferenceEquals(args.OldValue, args.NewValue)) {
+							args.OldValue.SetParent(null);
+							args.NewValue.SetParent(This);
+						}
+						break;
+					case ControllableListRangeAddedEventArgs<T> args:
+						foreach (var node in args.Values)
+							node.SetParent(This);
+						break;
+					case ControllableListRangeRemovedEventArgs<T> args:
+						foreach (var value in args.Values)
+							value.SetParent(null);
+						break;
+					case ControllableListRangeReplacedEventArgs<T> args:
+						foreach (var (old, @new) in args.OldValues.IndexJoin(args.NewValues))
+							if (!ReferenceEquals(old, @new)) {
+								old.SetParent(null);
+								@new.SetParent(This);
+							}
+						break;
+				}
+			};
+		}
+
+		public event ControllableListChangedEventHandler ChildrenChanged {
+			add => PrivateChildren.ListChanged += value;
+			remove => PrivateChildren.ListChanged -= value;
+		}
+
+		/// <summary>
+		///     Parent node. Note that when modified, the <see cref="Children" /> of the new and old parent node will be
+		///     automatically synchronized.
+		/// </summary>
+		public override T? Parent {
+			get => base.Parent;
+			set {
+				if (Equals(base.Parent, value))
+					return;
+				if (base.Parent is not null) {
+					base.Parent._updateParentOnChildrenChange = false;
+					base.Parent._children.Remove(This);
+					base.Parent._updateParentOnChildrenChange = true;
+				}
+				if (value is not null) {
+					value._updateParentOnChildrenChange = false;
+					value._children.Add(This);
+					value._updateParentOnChildrenChange = true;
+				}
+				SetParent(value);
+			}
+		}
+
+		/// <summary>
+		///     Child nodes collection. Note that when the collection is modified, the <see cref="Parent" /> of the added or
+		///     removed nodes will be automatically synchronized.<br />
+		///     Caution: DO NOT use indexers to reorder, which may break the synchronization with <see cref="Parent" />, thus all
+		///     operations that attempt to add or replace with nodes that already exist in <see cref="Children" /> will result in
+		///     <see cref="InvalidOperationException" />.<br />
+		///     If reordering is really needed, use use <see cref="IExtendedList{T}.Swap" />,
+		///     <see cref="IExtendedList{T}.Reverse()" /> or <see cref="IExtendedList{T}.Sort()" /> instead.
+		/// </summary>
+		public new IExtendedList<T> Children => PrivateChildren;
+
+		private ControllableList<T> PrivateChildren => (ControllableList<T>)_children;
+	}
+
+	/// <summary>
+	///     Tree node that ensures the synchronization between <c>Children</c> and <c>Parent</c>.
+	///     If inheritance required, use <see cref="TreeNode{T}" /> instead.
+	/// </summary>
+	public sealed class TreeNode : TreeNode<TreeNode> {
+		public TreeNode() { }
+
+		public TreeNode(TreeNode? parent) : base(parent) { }
+	}
+
+	/// <summary>
+	///     Tree node with value that ensures the synchronization between <c>Children</c> and <c>Parent</c>.
+	///     If inheritance required, use <see cref="TreeNode{T}" /> instead.
+	/// </summary>
+	public sealed class ValuedTreeNode<T> : TreeNode<ValuedTreeNode<T>> {
+		public ValuedTreeNode(T value) : this(value, null) { }
+
+		public ValuedTreeNode(T value, ValuedTreeNode<T>? parent) : base(parent) => Value = value;
+
+		public T Value { get; set; }
+	}
+}
