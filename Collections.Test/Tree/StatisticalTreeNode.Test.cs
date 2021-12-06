44c0021618b33dad589bfbdbfff631f48c9010a7
true_mogician <925269713@qq.com>
2021-12-06 22:07:59 +0800

    test: StatisticalTreeNode

---
+++ Collections.Test/Tree/StatisticalTreeNode.Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TrueMogician.Extensions.Collections.Tree;
+
+namespace Collections.Tree.Test {
+	public class StatisticalTreeNodeTests {
+		public static ValuedStatisticalTreeNode<string?> CreateNewNode(string? value = null) => new(value);
+
+		public ValuedStatisticalTreeNode<string?> Root = CreateNewNode("root");
+
+		public ValuedStatisticalTreeNode<string?>[] Nodes = new ValuedStatisticalTreeNode<string?>[8];
+
+		[SetUp]
+		public void BuildTree() {
+			for (var i = 0; i < Nodes.Length; ++i)
+				Nodes[i] = CreateNewNode($"{i}");
+			Nodes[0].Parent = Root;
+			Nodes[1].Parent = Root;
+			Nodes[2].Parent = Root;
+			Nodes[3].Parent = Nodes[1];
+			Nodes[4].Parent = Nodes[1];
+			Nodes[5].Parent = Nodes[3];
+			Nodes[6].Parent = Nodes[3];
+			Nodes[7].Parent = Nodes[2];
+		}
+
+		[Test]
+		public void StatisticsTest() {
+			Assert.AreEqual(Root.Depth, 0);
+			Assert.AreEqual(Root.Height, 4);
+			Assert.AreEqual(Root.Size, 9);
+			Assert.AreEqual(Nodes[3].Depth, 2);
+			Assert.AreEqual(Nodes[3].Height, 2);
+			Assert.AreEqual(Nodes[3].Size, 3);
+		}
+
+		[Test]
+		public void UtilitiesTest() {
+			Assert.IsTrue(Root.IsRoot);
+			Assert.IsTrue(Nodes[0].IsLeaf);
+			Assert.IsTrue(Root.IsAncestorOf(Nodes[7]));
+			Assert.IsFalse(Nodes[5].IsChildOf(Nodes[0]));
+			Assert.AreEqual(new[] {Nodes[0], Nodes[5], Nodes[6], Nodes[4], Nodes[7]}, Root.Leaves.ToArray());
+			Assert.AreEqual(new[] {Nodes[3], Nodes[1], Root}, Nodes[6].Ancestors.ToArray());
+			Assert.AreSame(Nodes[1], ValuedStatisticalTreeNode<string?>.GetLatestCommonAncestor(Nodes[5], Nodes[4]));
+		}
+	}
+}
