7b20f5339b76955af12a1dc69d5c1e4cf1945a89
true_mogician <925269713@qq.com>
2021-12-07 01:29:21 +0800

    feat: BugFoundException

---
+++ Exceptions/BugFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace TrueMogician.Exceptions {
+	/// <summary>
+	///     The exception that is thrown when code runs into somewhere unexpected, which means a potential bug is found.
+	/// </summary>
+	public class BugFoundException : ExceptionWithDefaultMessage {
+		public BugFoundException(string? message = null, Exception? innerException = null) : base(message, innerException) { }
+
+		public string? BugInformation { get; init; }
+
+		protected override string? DefaultMessage {
+			get {
+				var builder = new StringBuilder("Congratulations on finding a bug! ");
+				if (BugInformation is not null) {
+					builder.AppendLine("Here's the information:");
+					builder.AppendLine(BugInformation);
+				}
+				if (!string.IsNullOrEmpty(HelpLink))
+					builder.Append($"Maybe you can contact the careless author by {HelpLink}");
+				return builder.ToString();
+			}
+		}
+	}
+}
