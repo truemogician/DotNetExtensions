84a0ae96043eb0ec442f8bd0a1e4f9707db84091
true_mogician <925269713@qq.com>
2021-12-25 21:53:07 +0800

    feat: tree wrapper

---
+++ Collections/Tree/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TrueMogician.Extensions.Collections.Tree {
+	public class Tree<T> {
+		public Tree(T root) => Root = root;
+
+		public T Root { get; }
+
+		public override string? ToString() => Root?.ToString();
+
+		public override int GetHashCode() => Root?.GetHashCode() ?? throw new NullReferenceException("Root is null");
+
+#if NET5_0_OR_GREATER
+		public override bool Equals(object? obj) => obj is Tree<T> tree ? Equals(Root, tree.Root) : Equals(Root, obj);
+#else
+		public override bool Equals(object obj) => obj is Tree<T> tree ? Equals(Root, tree.Root) : Equals(Root, obj);
+#endif
+
+		public static implicit operator Tree<T>(T root) => new(root);
+
+		public static implicit operator T(Tree<T> tree) => tree.Root;
+	}
+}
