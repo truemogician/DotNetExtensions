f5d52395adb6a712c59e2dce15f66aaddc795a2c
true_mogician <925269713@qq.com>
2022-01-01 19:02:06 +0800

    feat: IEnumerator<T>.Move

--- Enumerator/EnumeratorExtensions.cs
+++ Enumerator/EnumeratorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Enumerator {
 	public static class EnumeratorExtensions {
@@ -11,6 +12,14 @@ namespace Enumerator {
 		public static T MoveNextAndGet<T>(this IEnumerator<T> enumerator) {
 			enumerator.MoveNext();
 			return enumerator.Current;
+		}
+
+		public static IEnumerator<T> Move<T>(this IEnumerator<T> enumerator, int step = 1) {
+			if (step < 0)
+				throw new ArgumentOutOfRangeException(nameof(step), $"Parameter {nameof(step)} cannot be negative");
+			while (step-- > 0)
+				enumerator.MoveNext();
+			return enumerator;
 		}
 
 		public static IExtendedEnumerator<T> ToExtended<T>(this IEnumerator<T> enumerator) => new ExtendedEnumerator<T>(enumerator);
