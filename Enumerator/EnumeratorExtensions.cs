6458a7d6ba73a934a2dc9ae817a0eb3167659a55
true_mogician <925269713@qq.com>
2022-01-01 19:01:31 +0800

    feat: EnumeratorExtensions

---
+++ Enumerator/EnumeratorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Enumerator {
+	public static class EnumeratorExtensions {
+		public static T GetAndMoveNext<T>(this IEnumerator<T> enumerator) {
+			var result = enumerator.Current;
+			enumerator.MoveNext();
+			return result;
+		}
+
+		public static T MoveNextAndGet<T>(this IEnumerator<T> enumerator) {
+			enumerator.MoveNext();
+			return enumerator.Current;
+		}
+
+		public static IExtendedEnumerator<T> ToExtended<T>(this IEnumerator<T> enumerator) => new ExtendedEnumerator<T>(enumerator);
+
+		public static IExtendedEnumerator<T> AsExtended<T>(this IEnumerator<T> enumerator) => enumerator is IExtendedEnumerator<T> e ? e : new ExtendedEnumerator<T>(enumerator);
+	}
+}
