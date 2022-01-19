e7e10b91d050132918df0da10ebc5cf19351a038
true_mogician <925269713@qq.com>
2022-01-20 00:25:59 +0800

    feat: T[].GetEnumerable

---
+++ Enumerable/ArrayExtensions.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace TrueMogician.Extensions.Enumerable {
+	public static class ArrayExtensions {
+		public static IEnumerator<T> GetGenericEnumerator<T>(this T[] array) => (IEnumerator<T>)array.GetEnumerator();
+	}
+}
