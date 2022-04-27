43ce14ca05ef98ea8f33be3f49307c97bfc80504
true_mogician <925269713@qq.com>
2022-04-27 20:45:00 +0800

    feat: GetGenericEnumerator

---
+++ Array/ArrayExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TrueMogician.Extensions.Array {
+	public static class ArrayExtensions {
+		public static IEnumerator<T> GetGenericEnumerator<T>(this T[] array) => array.AsEnumerable().GetEnumerator();
+	}
+}
