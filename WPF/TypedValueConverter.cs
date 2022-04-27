c3a203001285434bc2a4e3a98d563c3c869052b1
true_mogician <925269713@qq.com>
2022-04-28 02:10:20 +0800

    feat: TypedValueConverter

---
+++ WPF/TypedValueConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using TrueMogician.Exceptions;
+
+namespace TrueMogician.Extensions.WPF;
+
+public interface IValueConverter<TValue, TResult> : IValueConverter {
+	object? IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+		if (!targetType.IsAssignableTo(typeof(TResult)))
+			throw new InvariantTypeException(typeof(TResult), targetType);
+		if (value is TValue v)
+			return Convert(v, parameter, culture);
+		throw new InvariantTypeException(typeof(TValue), value.GetType());
+	}
+
+	object? IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+		if (!targetType.IsAssignableTo(typeof(TValue)))
+			throw new InvariantTypeException(typeof(TValue), targetType);
+		if (value is TResult v)
+			return ConvertBack(v, parameter, culture);
+		throw new InvariantTypeException(typeof(TResult), value.GetType());
+	}
+
+	protected TResult Convert(TValue value, object parameter, CultureInfo culture);
+
+	protected TValue ConvertBack(TResult value, object parameter, CultureInfo culture);
+}
+
+public interface IOneWayValueConverter<in TValue, out TResult> : IValueConverter {
+	object? IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+		if (!targetType.IsAssignableTo(typeof(TResult)))
+			throw new InvariantTypeException(typeof(TResult), targetType);
+		if (value is TValue v)
+			return Convert(v, parameter, culture);
+		throw new InvariantTypeException(typeof(TValue), value.GetType());
+	}
+
+	object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+
+	protected TResult Convert(TValue value, object parameter, CultureInfo culture);
+}
