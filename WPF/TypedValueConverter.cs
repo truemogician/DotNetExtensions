e5d22e4cfa14fb6c9eb1a92de274dd028f2e8a05
true_mogician <925269713@qq.com>
2022-04-28 02:20:30 +0800

    feat: add .net framework 4.6.2+ support

--- WPF/TypedValueConverter.cs
+++ WPF/TypedValueConverter.cs
@@ -5,6 +5,7 @@ using TrueMogician.Exceptions;
 
 namespace TrueMogician.Extensions.WPF;
 
+#if NET5_0_OR_GREATER
 public interface IValueConverter<TValue, TResult> : IValueConverter {
 	object? IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture) {
 		if (!targetType.IsAssignableTo(typeof(TResult)))
@@ -40,3 +41,26 @@ public interface IOneWayValueConverter<in TValue, out TResult> : IValueConverter
 
 	protected TResult Convert(TValue value, object parameter, CultureInfo culture);
 }
+#elif NET462_OR_GREATER
+public abstract class ValueConverter<TValue, TResult> : IValueConverter {
+	public object? Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+		if (!typeof(TResult).IsAssignableFrom(targetType))
+			throw new InvariantTypeException(typeof(TResult), targetType);
+		if (value is TValue v)
+			return Convert(v, parameter, culture);
+		throw new InvariantTypeException(typeof(TValue), value.GetType());
+	}
+
+	public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+		if (!typeof(TValue).IsAssignableFrom(targetType))
+			throw new InvariantTypeException(typeof(TValue), targetType);
+		if (value is TResult v)
+			return ConvertBack(v, parameter, culture);
+		throw new InvariantTypeException(typeof(TResult), value.GetType());
+	}
+
+	protected abstract TResult Convert(TValue value, object parameter, CultureInfo culture);
+
+	protected abstract TValue ConvertBack(TResult value, object parameter, CultureInfo culture);
+}
+#endif
