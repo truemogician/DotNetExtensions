644cef64101529f347e73cf6e0adaef4bd22d311
true_mogician <truemogician@gmail.com>
2024-06-06 14:32:07 -0700

    doc(String): add code doc

--- String/StringExtensions.cs
+++ String/StringExtensions.cs
@@ -4,10 +4,31 @@ using System.Runtime.CompilerServices;
 using TrueMogician.Exceptions;
 
 namespace TrueMogician.Extensions.String {
+	/// <summary>
+	/// Extension class for <see cref="string"/>
+	/// </summary>
 	public static class StringExtensions {
+        /// <summary>
+        /// Convert <paramref name="str"/> to <typeparamref name="T"/>.
+        /// This function has special treatment for <see cref="Enum"/> and types implementing <see cref="IConvertible"/>,
+        /// otherwise it'll look for constructor accepting only one <see cref="string" /> as parameter.
+        /// </summary>
+        /// <typeparam name="T">The type <paramref name="str"/> will be converted to.</typeparam>
+        /// <param name="str">The current string.</param>
+		/// <returns>A variable of <typeparamref name="T"/> converted from <paramref name="str"/>.</returns>
+        /// <exception cref="TypeException"></exception>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static T ConvertTo<T>(this string str) => (T)ConvertTo(str, typeof(T));
 
+		/// <summary>
+		/// Convert <paramref name="str"/> to <paramref name="targetType"/>.
+		/// This function has special treatment for <see cref="Enum"/> and types implementing <see cref="IConvertible"/>,
+		/// otherwise it'll look for constructor accepting only one <see cref="string" /> as parameter.
+		/// </summary>
+		/// <param name="str">The current string.</param>
+		/// <param name="targetType">The type <paramref name="str"/> will be converted to.</param>
+		/// <returns>A variable of <paramref name="targetType"/> converted from <paramref name="str"/>.</returns>
+		/// <exception cref="TypeException"></exception>
 		public static object ConvertTo(this string str, Type targetType) {
 			if (typeof(Enum).IsAssignableFrom(targetType))
 				return Enum.Parse(targetType, str);
