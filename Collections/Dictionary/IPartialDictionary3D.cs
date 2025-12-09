05b2bb4e8db46e718370d40d16909e6531adcb86
true_mogician <truemogician@gmail.com>
2025-12-08 20:11:56 -0800

    docs(Collections): add docs for `IPartialDictionary3D` and `TuplePartialDictionary3D`

--- Collections/Dictionary/IPartialDictionary3D.cs
+++ Collections/Dictionary/IPartialDictionary3D.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 #if !NETSTANDARD2_0
 using System.Linq;
 #endif
@@ -9,7 +10,21 @@ using System.Linq;
 
 namespace TrueMogician.Extensions.Collections.Dictionary;
 
+/// <summary>
+///     Represents a generic collection of values indexed by a composite key, allowing partial access via the first key component.
+/// </summary>
+/// <typeparam name="TKey1">The type of the first component of the keys in the dictionary.</typeparam>
+/// <typeparam name="TKey2">The type of the second component of the keys in the dictionary.</typeparam>
+/// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
 public interface IPartialDictionary3D<TKey1, TKey2, TValue> : IDictionary3D<TKey1, TKey2, TValue> {
+	/// <summary>
+	///     Gets an <see cref="ICollection{T}" /> containing the unique first key components in the
+	///     <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />.
+	/// </summary>
+	/// <returns>
+	///     An <see cref="ICollection{T}" /> containing the unique first key components of the object that implements
+	///     <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />.
+	/// </returns>
 	ICollection<TKey1> FirstKeys
 #if NETSTANDARD2_0
 		{ get; }
@@ -17,6 +32,15 @@ public interface IPartialDictionary3D<TKey1, TKey2, TValue> : IDictionary3D<TKey
 		=> Keys.Select(k => k.Item1).Distinct().ToArray();
 #endif
 
+	/// <summary>
+	///     Gets a collection of second key components and values associated with the specified first key component.
+	/// </summary>
+	/// <param name="key1">The first component of the key of the elements to retrieve.</param>
+	/// <exception cref="KeyNotFoundException">The property is retrieved and <paramref name="key1" /> is not found.</exception>
+	/// <returns>
+	///     An <see cref="IDictionary{TKey, TValue}" /> containing the second key components and values associated with
+	///     <paramref name="key1" />.
+	/// </returns>
 	IDictionary<TKey2, TValue> this[TKey1 key1]
 #if NETSTANDARD2_0
 		{ get; }
@@ -24,6 +48,16 @@ public interface IPartialDictionary3D<TKey1, TKey2, TValue> : IDictionary3D<TKey
 		=> TryGetValues(key1, out var dict) ? dict : throw new KeyNotFoundException();
 #endif
 
+	/// <summary>
+	///     Adds the specified collection of key/value pairs associated with the specified first key component.
+	/// </summary>
+	/// <param name="key1">The object to use as the first component of the key of the elements to add.</param>
+	/// <param name="values">The collection of second key components and values to add.</param>
+	/// <exception cref="ArgumentException">
+	///     An element with the same composite key already exists in the
+	///     <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />.
+	/// </exception>
+	/// <exception cref="NotSupportedException">The <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" /> is read-only.</exception>
 	void Add(TKey1 key1, IEnumerable<KeyValuePair<TKey2, TValue>> values)
 #if NETSTANDARD2_0
 		;
@@ -34,6 +68,17 @@ public interface IPartialDictionary3D<TKey1, TKey2, TValue> : IDictionary3D<TKey
 	}
 #endif
 
+	/// <summary>
+	///     Determines whether the <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" /> contains any elements with the
+	///     specified first key component.
+	/// </summary>
+	/// <param name="key1">
+	///     The first component of the key to locate in the <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />.
+	/// </param>
+	/// <returns>
+	///     <see langword="true" /> if the <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" /> contains any elements with
+	///     the specified first key component; otherwise, <see langword="false" />.
+	/// </returns>
 	bool ContainsKey(TKey1 key1)
 #if NETSTANDARD2_0
 		;
@@ -41,6 +86,12 @@ public interface IPartialDictionary3D<TKey1, TKey2, TValue> : IDictionary3D<TKey
 		=> Keys.Any(k => Comparer1.Equals(k.Item1, key1));
 #endif
 
+	/// <summary>
+	///     Removes all elements with the specified first key component from the <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />.
+	/// </summary>
+	/// <param name="key1">The first component of the key of the elements to remove.</param>
+	/// <exception cref="NotSupportedException">The <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" /> is read-only.</exception>
+	/// <returns>The number of elements removed from the <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />.</returns>
 	int Remove(TKey1 key1)
 #if NETSTANDARD2_0
 		;
@@ -50,6 +101,18 @@ public interface IPartialDictionary3D<TKey1, TKey2, TValue> : IDictionary3D<TKey
 			.Count(b => b);
 #endif
 
+	/// <summary>
+	///     Gets the collection of values associated with the specified first key component.
+	/// </summary>
+	/// <param name="key1">The first component of the key whose values to get.</param>
+	/// <param name="values">
+	///     When this method returns, the dictionary of second key components and values associated with the
+	///     specified first key component, if the key is found; otherwise, an empty dictionary.
+	/// </param>
+	/// <returns>
+	///     <see langword="true" /> if the <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" /> contains elements with the
+	///     specified first key component; otherwise, <see langword="false" />.
+	/// </returns>
 	bool TryGetValues(TKey1 key1, out IDictionary<TKey2, TValue> values)
 #if NETSTANDARD2_0
 		;
--- Collections/Dictionary/TuplePartialDictionary3D.cs
+++ Collections/Dictionary/TuplePartialDictionary3D.cs
@@ -8,6 +8,11 @@ using System.Linq;
 
 namespace TrueMogician.Extensions.Collections.Dictionary;
 
+/// <summary>
+///     Implementation of <see cref="IPartialDictionary3D{TKey1, TKey2, TValue}" /> based on
+///     <see cref="TupleDictionary3D{TKey1, TKey2, TValue}" />.
+/// </summary>
+/// <inheritdoc cref="IPartialDictionary3D{TKey1, TKey2, TValue}" />
 public class TuplePartialDictionary3D<TKey1, TKey2, TValue>
 	: TupleDictionary3D<TKey1, TKey2, TValue>, IPartialDictionary3D<TKey1, TKey2, TValue> {
 	public TuplePartialDictionary3D() { }
@@ -19,22 +24,28 @@ public class TuplePartialDictionary3D<TKey1, TKey2, TValue>
 
 	public TuplePartialDictionary3D(IDictionary3D<TKey1, TKey2, TValue> other) : base(other) { }
 
+	/// <inheritdoc/>
 	public ICollection<TKey1> FirstKeys => Keys.Select(k => k.Item1).Distinct().ToArray();
 
-    public IDictionary<TKey2, TValue> this[TKey1 key1]
+	/// <inheritdoc/>
+	public IDictionary<TKey2, TValue> this[TKey1 key1]
 		=> TryGetValues(key1, out var dict) ? dict : throw new KeyNotFoundException();
 
-	public void Add(TKey1 key1, IEnumerable<KeyValuePair<TKey2, TValue>> values) {
+	/// <inheritdoc/>
+    public void Add(TKey1 key1, IEnumerable<KeyValuePair<TKey2, TValue>> values) {
 		foreach (var p in values)
 			Add(key1, p.Key, p.Value);
 	}
 
-	public bool ContainsKey(TKey1 key1) => Keys.Any(k => Comparer1.Equals(k.Item1, key1));
+	/// <inheritdoc/>
+    public bool ContainsKey(TKey1 key1) => Keys.Any(k => Comparer1.Equals(k.Item1, key1));
 
-	public int Remove(TKey1 key1) => Keys.Where(k => Comparer1.Equals(k.Item1, key1))
+	/// <inheritdoc/>
+    public int Remove(TKey1 key1) => Keys.Where(k => Comparer1.Equals(k.Item1, key1))
 		.Select(k => Remove(k.Item1, k.Item2))
 		.Count(b => b);
 
+	/// <inheritdoc/>
 	public bool TryGetValues(TKey1 key1, out IDictionary<TKey2, TValue> values) {
 		values = this.Where(t => Comparer1.Equals(t.Item1, key1))
 			.ToDictionary(t => t.Item2, t => t.Item3);
