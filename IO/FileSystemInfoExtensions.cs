74b3c08566589cc890d3e10385f20aeff5f99769
true_mogician <truemogician@gmail.com>
2025-12-12 22:03:11 -0800

    feat(IO): add more extension methods

--- IO/FileSystemInfoExtensions.cs
+++ IO/FileSystemInfoExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace TrueMogician.Extensions.IO;
 
@@ -13,5 +15,40 @@ public static class FileSystemInfoExtensions {
 		public string RelativeTo(string @base)
 			=> Path.GetRelativePath(Path.GetFullPath(@base), self.FullName);
 #endif
+	}
+
+	extension(FileInfo self) {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Rename(string name)
+			=> self.MoveTo(self.Directory!.ChildPath(name));
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void RenameWithoutExtension(string name, string? newExt = null) {
+			string newName = newExt is null ? name + self.Extension : name + newExt;
+			self.MoveTo(self.Directory!.ChildPath(newName));
+		}
+	}
+
+	extension(DirectoryInfo self) {
+		public long Size =>
+#if NETSTANDARD2_0
+			self.EnumerateFiles("*", SearchOption.AllDirectories)
+#else
+			self.EnumerateFiles("*", new EnumerationOptions {
+				AttributesToSkip = 0,
+				RecurseSubdirectories = true
+			})
+#endif
+				.Sum(f => f.Length);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public string ChildPath(string subPath) => Path.Combine(self.FullName, subPath);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public string ResolveRelative(string relativePath) => Path.GetFullPath(Path.Combine(self.FullName, relativePath));
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Rename(string baseName)
+			=> self.MoveTo(self.Parent!.ChildPath(baseName));
 	}
 }
