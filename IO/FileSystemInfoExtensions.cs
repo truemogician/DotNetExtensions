1a28ef2a6e4f5f3d68a05f826a03ffcdef6dba44
true_mogician <truemogician@gmail.com>
2025-12-12 17:36:17 -0800

    feat(IO): add project IO

---
+++ IO/FileSystemInfoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace TrueMogician.Extensions.IO;
+
+public static class FileSystemInfoExtensions {
+	extension(FileSystemInfo self) {
+		public string NameWithoutExtension => Path.GetFileNameWithoutExtension(self.Name);
+
+#if !NETSTANDARD2_0
+		public string RelativeTo(DirectoryInfo @base)
+			=> Path.GetRelativePath(@base.FullName, self.FullName);
+
+		public string RelativeTo(string @base)
+			=> Path.GetRelativePath(Path.GetFullPath(@base), self.FullName);
+#endif
+	}
+}
